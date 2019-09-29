41cfd6b5706490a715c1eb2cab19efa4039c2a38
haoweiquan <haoweiquan@tencent.com>
2019-09-29 22:11:56 +0800

    add

--- apps/h5/src/App.js
+++ apps/h5/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter , Switch, Route, Redirect } from "react-router-dom";
+import Index from './pages/Index1'
+import Index2 from './pages/Index2'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <div>
+          <Switch>
+            <Route path="/index1"><Index /></Route>
+            <Route path="/index2"><Index2 /></Route>
+          </Switch>
+        </div>
+      </HashRouter>
     </div>
   );
 }
---
+++ apps/h5/src/pages/Index2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+class Index1 extends Component {
+  componentWillMount () {
+
+  }
+
+  render() {
+    return <div>222</div>
+  }
+}
+
+export default withRouter(Index1)
