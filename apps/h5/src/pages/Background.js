e6b87f9d63191e3be84ccff5f1038bacd6b2bd8c
PandaQ <331748458@qq.com>
2019-10-10 13:49:20 +0800

    add

--- apps/h5/src/pages/Background.js
+++ apps/h5/src/pages/Background.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 import './Background.scss'
 
-if (window.DeviceOrientationEvent) {
-  window.addEventListener('deviceorientation', DeviceOrientationHandler, false);
-} else {
-  alert("您的浏览器不支持HTML5 DeviceOrientation接口");
-}
-
-function DeviceOrientationHandler(event) {
-  alert(event.alpha)
-}
-
 class Page extends Component {
   constructor(props) {
     super(props)
-    this.state = { event: { alpha: 0, beta: 0, gamma: 0 }, index: 0 }
+    this.state = { event: { alpha: 0, beta: 0, gamma: 0 } }
   }
   componentDidMount() {
-
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener('deviceorientation', this.deviceOrientationHandler, false);
+    }
   }
 
   deviceOrientationHandler = (event) => {
-    const { index } = this.state;
-    alert("222");
-    console.log(event)
-    this.setState({ event, index: index + 1 })
+    this.setState({ event: { alpha: this.getOffset(event.alpha), beta: this.getOffset(event.beta), gamma: this.getOffset(event.gamma) } })
+  }
+
+  getOffset(num) {
+    let res = 0
+    if(num > 240) {
+      res = num - 360
+    } else{
+      res = num;
+    }
+    return parseInt(res) ;
   }
 
   render() {
     const { step } = this.props;
-    const { event, index } = this.state;
+    const { event } = this.state;
     return <div className='main-background'>
-      <img style={{ position: "absolute", left: '0', top: '0', width: '100%', height: '100%', objectFit: 'cover' }} src={require('../images/background.jpg')} />
+      <img style={{ transform: `translate(${event.alpha}px, ${event.beta}px)`, position: "absolute", left: '0', top: '0', width: '100%', height: '100%', objectFit: 'cover' }} src={require('../images/background.jpg')} />
       <img style={{ position: "absolute", right: '0', top: '0', width: '190px', height: '307px' }} className="start1" src={require('../images/bk1.png')} />
       <img style={{ position: "absolute", left: '150px', top: '200px', width: '187px', height: '127px' }} className="start2" src={require('../images/bk2.png')} />
       <img style={{ position: "absolute", right: '50px', top: '100px', width: '29px', height: '188px' }} className="start3" src={require('../images/bk3.png')} />
@@ -39,7 +38,6 @@ class Page extends Component {
       <div style={{ position: "absolute", right: '20px', top: '50px', color: '#ffffff' }}>alpha{event.alpha}</div>
       <div style={{ position: "absolute", right: '20px', top: '80px', color: '#ffffff' }}>beta{event.beta}</div>
       <div style={{ position: "absolute", right: '20px', top: '110px', color: '#ffffff' }}>gamma{event.gamma}</div>
-      <div style={{ position: "absolute", right: '20px', top: '140px', color: '#ffffff' }}>aindex {index}</div>
     </div>
   }
 }
